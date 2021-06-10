defb0b347612deb0f8d75ff90a0870c28949b132
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-10 16:44:59 +0700

    Tugas 27 JS
    Tugas ini Saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,15 @@
+// “No”,” rumah saya”,30, “A”
+// “Jalan pangeran Tirtayasa No”, 70
+function panggilString(){
+  var a = "No";
+  var b = " rumah saya "
+  var c =  30;
+  var d = " A"
+  var e = "Jalan pangeran Tirtayasa No ";
+  var f = 70;
+  hasil = a+b+String(c)+d;
+  console.log(hasil);
+  hasil2 = e+String(f)
+  console.log(hasil2);
+}
+panggilString()
